f3ef3d5224184579a0f0dfa8aa75ecfe213fa39c
0xE0D59 <0xE0D59@gmail.com>
2022-09-21 23:19:06 +0200

    1.10 Added Trundle (Q auto-attack resets)

--- Sussy.lua
+++ Sussy.lua
@@ -4,6 +4,7 @@
 	version 1.9
 	
 	Changelog:
+	-- 1.10: Added Trundle
 	-- 1.9: Added Dr Mundo
 	-- 1.8: Added Nautilus
 	-- 1.7:	Added Zilean
@@ -1773,5 +1774,40 @@ do
             print("Sussy " .. myHero.charName .. " loaded.")
         end
         -- Renata END
+		
+		-- Trundle START
+        if myHero.charName == "Trundle" then
+            Menu:Init()
+            Menu.w:Remove()
+            Menu.e:Remove()
+            Menu.r:Remove()
+            Menu.d:Remove()
+
+            Menu.q_combo = Menu.q:MenuElement({id = "combo", name = "Combo", value = true})
+            Menu.q_harass = Menu.q:MenuElement({id = "harass", name = "Harass", value = false})
+
+            Callback.Add(
+                "Tick",
+                function()
+                end
+            )
+            Orb:OnPostAttack(
+                function()
+                    local mode = Orb:GetMode()
+                    if
+                        Spells:IsReady(_Q) and
+                            ((mode == "Combo" and Menu.q_combo:Value()) or (mode == "Harass" and Menu.q_harass:Value()))
+                     then
+                        local target = Orb:GetTarget(300)
+                        if target then
+                            Control.CastSpell(HK_Q)
+                        end
+                    end
+                end
+            )
+
+            print("Sussy " .. myHero.charName .. " loaded.")
+        end
+        -- Trundle END
     end
 end
